bd8262d70728967376fe9e8e3e91247e2849be9e
refraincr <xinchunzhong4@gmail.com>
2026-07-17 17:59:45 +0800

    提取了提取添加历史记录和摘要方法

--- src/main/java/com/cos/demo/service/impl/DioServiceImpl.java
+++ src/main/java/com/cos/demo/service/impl/DioServiceImpl.java
@@ -52,70 +52,21 @@ public class DioServiceImpl implements RoleService {
 
             // 拼接Assistant信息
             StringBuilder answer = new StringBuilder();
-            // 拼接工具调用
-            Map<Integer, StringBuilder> toolArguments = new HashMap<>();
-            // 工具名称
-            Map<Integer, String> toolNames = new HashMap<>();
             stream.stream().forEach(chunk -> {
                 if (chunk.choices().isEmpty()) return;
                 ChatCompletionChunk.Choice choice = chunk.choices().getFirst();
                 ChatCompletionChunk.Choice.Delta delta = choice.delta();
                 // 普通文本
                 delta.content().ifPresent(answer::append);
-                // 工具调用
-                if (delta.toolCalls().isPresent()) {
-                    List<ChatCompletionChunk.Choice.Delta.ToolCall> toolCalls =
-                            delta.toolCalls().get();
-
-                    for (ChatCompletionChunk.Choice.Delta.ToolCall toolCall : toolCalls) {
-                        int index = (int) toolCall.index();
-
-                        toolCall.function().ifPresent(function -> {
-                            // 工具名称
-                            function.name().ifPresent(name -> toolNames.put(index, name));
-                            // 拼接增量的 JSON 参数字符串
-                            function.arguments().ifPresent(args->
-                                    toolArguments.computeIfAbsent(index, arg ->
-                                                    new StringBuilder()).append(args));
-                        });
-                    }
-                }
             });
 
-            // 工具处理
-            for (Map.Entry<Integer, StringBuilder> entry : toolArguments.entrySet()) {
-                int index = entry.getKey();
-                String functionName = toolNames.get(index);
-                if ("StartIdiom".equals(functionName)) {
-                    addPromptAndResponse();
-                }
-
-            }
             String finalAnswer = answer.toString();
 
             //保存user,Assistant信息
-            history.add(new ChatMessage("user", dioBO.getMsg()));
-            history.add(new ChatMessage("assistant", finalAnswer));
+            addUserAndAssistant(dioBO.getMsg(), finalAnswer, history);
 
             // 若要做摘要
-            if (memoryService.shouldSummary(dioBO.getSessionId(),history)) {
-                // 防止重复摘要，只摘要最新的
-                MemoryState state = stateManager.getState(dioBO.getSessionId());
-                List<ChatMessage> newHistory =
-                        history.subList(
-                                state.getSummarizedIndex(),
-                                history.size()
-                        );
-                // 异步获取摘要
-                memoryService.summaryAsync(
-                        dioBO.getSessionId(),
-                        new ArrayList<>(newHistory),
-                        history.size()
-                );
-
-                // 更新state
-                state.setSummarizedIndex(history.size());
-            }
+            if (memoryService.shouldSummary(dioBO.getSessionId(),history)) addSummary(dioBO.getSessionId(),history);
 
             emitter.complete();
         } catch (Exception e) {
@@ -123,13 +74,9 @@ public class DioServiceImpl implements RoleService {
         }
     }
 
-    private void addPromptAndResponse() {
-        sceneManager.changeScene("IDIOM");
-    }
-
     @Override
     public String talkNoStream(DioBO dioBO) {
-        String answer = "";
+        String answer;
         String assistantRes = "";
         while (true) {
             // 构造参数
@@ -147,28 +94,22 @@ public class DioServiceImpl implements RoleService {
             ChatCompletionCreateParams params = builder.build();
 
             // 模型回复
-            ChatCompletion completion = client.chat()
+            ChatCompletionMessage message = client.chat()
                     .completions()
-                    .create(params);
-
-            ChatCompletionMessage message = completion.choices()
+                    .create(params)
+                    .choices()
                     .getFirst()
                     .message();
 
             // 是否使用工具
             List<ChatCompletionMessageToolCall> toolCalls =
                     message.toolCalls().orElse(Collections.emptyList());
-
             if (!toolCalls.isEmpty()) {
                 for (ChatCompletionMessageToolCall toolCall : toolCalls) {
                     String name = toolCall.function()
                             .orElseThrow()
                             .function()
                             .name();
-                    String arguments = toolCall.function()
-                            .orElseThrow()
-                            .function()
-                            .arguments();
                     // 暂时调用，展示可行性
                     if ("StartIdiom".equals(name)) {
                         addPromptAndResponse();
@@ -181,25 +122,41 @@ public class DioServiceImpl implements RoleService {
             answer = message.content().orElse("");
 
             // 消息加入历史
-            history.add(new ChatMessage("user", dioBO.getMsg()));
-            history.add(new ChatMessage("assistant", answer));
+            addUserAndAssistant(dioBO.getMsg(), answer, history);
 
             // 是否摘要
-            if (memoryService.shouldSummary(dioBO.getSessionId(),history)) {
-                // 防止重复获取摘要
-                MemoryState state = stateManager.getState(dioBO.getSessionId());
-                List<ChatMessage> newHistory =
-                        history.subList(state.getSummarizedIndex(),history.size());
-                memoryService.summaryAsync(dioBO.getSessionId(),
-                        new ArrayList<>(newHistory), // 小坑
-                        history.size());
-                // 更新偏移量
-                state.setSummarizedIndex(history.size());
-            }
+            if (memoryService.shouldSummary(dioBO.getSessionId(),history)) addSummary(dioBO.getSessionId(),history);
             break;
         }
 
 
         return answer;
+    }
+
+    private void addPromptAndResponse() {
+        sceneManager.changeScene("IDIOM");
+    }
+
+    private void addUserAndAssistant(String userMsg, String assistantMsg,List<ChatMessage> history) {
+        history.add(new ChatMessage("user", userMsg));
+        history.add(new ChatMessage("assistant", assistantMsg));
+    }
+
+    private void addSummary(String sessionId, List<ChatMessage> history) {
+        // 防止重复摘要，只摘要最新的
+        MemoryState state = stateManager.getState(sessionId);
+        List<ChatMessage> newHistory =
+                history.subList(
+                        state.getSummarizedIndex(),
+                        history.size()
+                );
+        // 异步获取摘要
+        memoryService.summaryAsync(
+                sessionId,
+                new ArrayList<>(newHistory),
+                history.size()
+        );
+        // 更新state
+        state.setSummarizedIndex(history.size());
     }
 }
